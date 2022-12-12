1b4f8a99ab3b6904dbf5b81cec01227d06a1846b
Sasha Bogicevic <Sasha.Bogicevic@iohk.io>
2022-12-12 17:06:35 +0100

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+# check to see is git command line installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+
+
+cp   /etc/nixos/configuration.nix .
+cp   /etc/nixos/hardware-configuration.nix .
+cp  -R $HOME/.xmonad/* .
+cp  -R $HOME/.doom.d/* .
+cp  -R $HOME/.emacs.d/* .
+cp  $HOME/.xmobarrc .
+
+
+
+# Check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+
+# push to Github
+git add -u;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master
