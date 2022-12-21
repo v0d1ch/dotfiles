44471a1cb3f5193de330ab22eb1fa330b52db07e
Sasha Bogicevic <Sasha.Bogicevic@iohk.io>
2022-12-21 11:38:31 +0100

    New backup 2022-12-21 11:38:31

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -91,9 +91,9 @@ in
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    pulse.enable = false;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
