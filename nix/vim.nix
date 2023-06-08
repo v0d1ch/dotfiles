121e1c828a9dbc706ba38911c9fb655d907ffc1b
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-06-08 09:13:35 +0200

    New backup 2023-06-08 09:13:35

--- nix/vim.nix
+++ nix/vim.nix
@@ -46,7 +46,7 @@
               comment-nvim 
               alpha-nvim
               nvim-treesitter
-              nvim-tree-lua
+              nvim-spectre
             ]; 
             opt = [];
         };
@@ -80,7 +80,8 @@
           let g:solarized_borders = v:false
           let g:solarized_disable_background = v:false
 
-          colorscheme tokyonight
+          " colorscheme tokyonight
+          colorscheme PaperColor 
 
           set showcmd
           set clipboard=unnamedplus
@@ -309,30 +310,52 @@
                   post_hook = nil,
                   }
             require'alpha'.setup(require'alpha.themes.dashboard'.config)
-            require'nvim-treesitter.configs'.setup {}
-
-            -- disable netrw at the very start of your init.lua (strongly advised)
-            vim.g.loaded_netrw = 1
-            vim.g.loaded_netrwPlugin = 1
-            
-            -- set termguicolors to enable highlight groups
-            vim.opt.termguicolors = true
-            
-            -- empty setup using defaults
-            -- require("nvim-tree").setup()
-            
-            -- OR setup with some options
-            require("nvim-tree").setup({
-              -- sort_by = "case_sensitive",
-              view = {
-                width = 30,
-              },
-              renderer = {
-                group_empty = true,
-              },
-              filters = {
-                dotfiles = true,
-              },
+            require('spectre').setup()
+            require('illuminate').configure({
+                -- providers: provider used to get references in the buffer, ordered by priority
+                providers = {
+                    'regex',
+                    'lsp',
+                    'treesitter',
+                },
+                -- delay: delay in milliseconds
+                delay = 100,
+                -- filetype_overrides: filetype specific overrides.
+                -- The keys are strings to represent the filetype while the values are tables that
+                -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+                filetype_overrides = {},
+                -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+                filetypes_denylist = {
+                    'dirvish',
+                    'fugitive',
+                },
+                -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+                filetypes_allowlist = {},
+                -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+                -- See `:help mode()` for possible values
+                modes_denylist = {},
+                -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+                -- See `:help mode()` for possible values
+                modes_allowlist = {},
+                -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+                -- Only applies to the 'regex' provider
+                -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+                providers_regex_syntax_denylist = {},
+                -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+                -- Only applies to the 'regex' provider
+                -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+                providers_regex_syntax_allowlist = {},
+                -- under_cursor: whether or not to illuminate under the cursor
+                under_cursor = true,
+                -- large_file_cutoff: number of lines at which to use large_file_config
+                -- The `under_cursor` option is disabled when this cutoff is hit
+                large_file_cutoff = nil,
+                -- large_file_config: config to use for large files (based on large_file_cutoff).
+                -- Supports the same keys passed to .configure
+                -- If nil, vim-illuminate will be disabled for large files.
+                large_file_overrides = nil,
+                -- min_count_to_highlight: minimum number of matches required to perform highlighting
+                min_count_to_highlight = 1,
             })
           EOF
 
