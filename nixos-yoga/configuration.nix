0558c75fdc836eed92c94e87390ae0d30c52e657
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-11-30 20:19:48 +0100

    Add p10k-config folder and nixos-yoga config

--- nixos-yoga/configuration.nix
+++ nixos-yoga/configuration.nix
@@ -6,8 +6,8 @@ in
     [ 
       ./hardware-configuration.nix
       <home-manager/nixos>
-      ./system-packages.nix
-      ./nvim
+      /home/v0d1ch/code/dotfiles/system-packages.nix
+      /home/v0d1ch/code/dotfiles/nvim
     ];
 
   boot.loader.systemd-boot.enable = true;
@@ -118,10 +118,10 @@ in
   # HOME
   home-manager.useGlobalPkgs = true;
   home-manager.users.v0d1ch = { ... }: {
-    imports = [ ./home.nix ];
+    imports = [ /home/v0d1ch/code/dotfiles/home.nix ];
   };
   home-manager.users.root = { ... }: {
-    imports = [ ./home.nix ];
+    imports = [ /home/v0d1ch/code/dotfiles/home.nix ];
   };
 
   services.udev.packages = 
