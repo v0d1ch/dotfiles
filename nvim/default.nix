3b786b9fd6d18ea370b04fc3279db6b7a9e73781
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-12-08 08:16:10 +0100

    Some nice Git plugins

--- nvim/default.nix
+++ nvim/default.nix
@@ -26,12 +26,15 @@ in
        vimAlias = true;
        withNodeJs = true;
        plugins = with pkgs.vimPlugins; [
-            nvim-lspconfig
-            nvim-treesitter.withAllGrammars
-            plenary-nvim
             (fromGitHub "f59b0e9648254ccc125e5ddb411711a8438476b3" "master" "NeogitOrg/neogit")
             (fromGitHub "3dc498c9777fe79156f3d32dddd483b8b3dbd95f" "main" "sindrets/diffview.nvim")
             (fromGitHub "11a4d42244755505b66b15cd4496a150432eb5e3" "master" "rhysd/conflict-marker.vim")
+            (fromGitHub "36ff7abb6b60980338344982ad4cdf03f7961ecd" "master" "mbbill/undotree.vim")
+            (fromGitHub "6ef8c54fb526bf3a0bc4efb0b2fe8e6d9a7daed2" "release" "lewis6991/gitsigns.nvim")
+
+            nvim-lspconfig
+            nvim-treesitter.withAllGrammars
+            plenary-nvim
             nerdtree
             nerdcommenter
             awesome-vim-colorschemes
