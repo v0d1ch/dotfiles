3359968acfca9a95ba947789be6a78a8ac2615d9
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-03-01 16:49:58 +0100

    Bump neovide

--- nvim/default.nix
+++ nvim/default.nix
@@ -26,7 +26,7 @@ in
        vimAlias = true;
        withNodeJs = true;
        plugins = with pkgs.vimPlugins; [
-            (fromGitHub "ce0c369ccdba3f644a3b28f4c053421f435352c9" "master" "NeogitOrg/neogit")
+            (fromGitHub "0d0879b0045fb213c328126969a3317c0963d34a" "master" "NeogitOrg/neogit")
             (fromGitHub "3dc498c9777fe79156f3d32dddd483b8b3dbd95f" "main" "sindrets/diffview.nvim")
             (fromGitHub "11a4d42244755505b66b15cd4496a150432eb5e3" "master" "rhysd/conflict-marker.vim")
             (fromGitHub "36ff7abb6b60980338344982ad4cdf03f7961ecd" "master" "mbbill/undotree")
