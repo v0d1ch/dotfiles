be751878d310b56ec749f9373db52e3671e4bf60
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-11-24 10:55:09 +0100

    Remove zsh
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- modules/home.nix
+++ modules/home.nix
@@ -62,6 +62,7 @@
          # yubioath-flutter
          #  (haskell-language-server.override { supportedGhcVersions = [ "8107" ]; })
          protonvpn-gui
+         blesh
      ];
 
 
@@ -146,98 +147,6 @@ lor:magenta)%(authorname)%(color:reset)' --color=always";
      programs.direnv = {
        enable = true;
        nix-direnv.enable = true;
-     };
-
-     programs.zsh = {
-       enable = true;
-       dotDir = ".config/zsh";
-       shellAliases = {
-         ll = "ls --all --color=tty";
-       };
-
-       history = {
-             size = 10000000;
-             save = 10000000;
-             share = true;
-             path = "$HOME/.zsh_history";
-           };
-       autosuggestion.enable = true;
-       enableCompletion = true;
-       initContent = ''
-            bindkey '^F' autosuggest-accept
-            bindkey '^U' backward-kill-line
-            bindkey '^A' beginning-of-line
-            bindkey '^E' end-of-line
-            bindkey -v
-            setopt magic_equal_subst
-            eval $(thefuck --alias)
-            autopair-init
-            zstyle -e ':completion:*' special-dirs true
-            set +o prompt_cr +o prompt_sp
-
-
-        '';
-       plugins = with pkgs; [
-          {
-            name = "formarks";
-            src = fetchFromGitHub {
-              owner = "wfxr";
-              repo = "formarks";
-              rev = "8abce138218a8e6acd3c8ad2dd52550198625944";
-              sha256 = "1wr4ypv2b6a2w9qsia29mb36xf98zjzhp3bq4ix6r3cmra3xij90";
-            };
-            file = "formarks.plugin.zsh";
-          }
-          {
-            name = "zsh-syntax-highlighting";
-            src = fetchFromGitHub {
-              owner = "zsh-users";
-              repo = "zsh-syntax-highlighting";
-              rev = "0.6.0";
-              sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
-            };
-            file = "zsh-syntax-highlighting.zsh";
-          }
-          {
-            name = "zsh-abbrev-alias";
-            src = fetchFromGitHub {
-              owner = "momo-lab";
-              repo = "zsh-abbrev-alias";
-              rev = "637f0b2dda6d392bf710190ee472a48a20766c07";
-              sha256 = "16saanmwpp634yc8jfdxig0ivm1gvcgpif937gbdxf0csc6vh47k";
-            };
-            file = "abbrev-alias.plugin.zsh";
-          }
-          {
-            name = "zsh-autopair";
-            src = fetchFromGitHub {
-              owner = "hlissner";
-              repo = "zsh-autopair";
-              rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
-              sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-            };
-            file = "autopair.zsh";
-          }
-          {
-            name = "zsh-autocomplete";
-            src = fetchFromGitHub {
-              owner = "marlonrichert";
-              repo = "zsh-autocomplete";
-              rev = "6d059a3634c4880e8c9bb30ae565465601fb5bd2";
-              sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-            };
-          }
-          {
-           # will source zsh-autosuggestions.plugin.zsh
-           name = "zsh-autosuggestions";
-           src = pkgs.fetchFromGitHub {
-             owner = "zsh-users";
-             repo = "zsh-autosuggestions";
-             rev = "v0.4.0";
-             sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-           };
-         }
-       ];
      };
 
      programs.fzf = {
